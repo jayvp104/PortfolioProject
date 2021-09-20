4b975890f753ab94189da20cadc781a14fb4276c
jayvp104 <89482803+jayvp104@users.noreply.github.com>
2021-09-20 18:50:17 -0400

    Add files via upload
    Joined two tables and combined the data into a new table. Created a column to show weekly rolling average for various fields.

---
+++ Covid_data_rolling_average_sp.sql
@@ -0,0 +1,57 @@
+
+DROP TABLE if exists Portfolio.dbo.Covid_combined 
+Select dea.continent continent,
+	   dea.location location, 
+	   CONVERT(date, dea.date) date, 
+	   dea.population population, 
+	   dea.total_cases total_cases,
+	   dea.new_cases new_cases,
+	   ISNULL(CONVERT(float, dea.total_deaths), 0) total_deaths,
+	   ISNULL(dea.new_deaths, 0) new_deaths,
+	   CONVERT(bigint, vac.new_tests) new_tests,
+	   CONVERT(bigint, vac.total_tests) total_tests,
+	   CONVERT(bigint, vac.total_vaccinations) total_vaccinations,
+	   CONVERT(bigint, vac.new_vaccinations) new_vaccinations,
+	   CONVERT(bigint, vac.people_vaccinated) people_vaccinated
+INTO Portfolio.dbo.Covid_combined   
+from Portfolio.dbo.Covid_Death dea
+	JOIN Portfolio.dbo.covid_vaccine Vac 
+		ON dea.date =Vac.date 
+			AND dea.location = Vac.location;
+
+
+
+select * from Portfolio.dbo.Covid_combined
+
+Create PROC sp_weekly_rolling_average
+AS
+
+Select Continent, location, date, ISNULL(new_deaths, 0) new_deaths, 
+	CASE WHEN count(*) OVER(Partition by location Order by date ROWS 6 PRECEDING)> 6
+		THEN ISNULL(ROUND(AVG(new_deaths)OVER(Partition by location Order by date ROWS 6 PRECEDING), 2), 0)
+		ELSE 0
+		END AS Avg_New_deaths,
+	new_cases,
+	CASE WHEN count(*) OVER(Partition by location Order by date ROWS 6 PRECEDING)> 6
+		THEN ISNULL(Round(AVG(new_cases)OVER(Partition by location Order by date ROWS 6 PRECEDING), 2), 0)
+		ELSE 0
+		END AS Avg_New_Cases,
+	new_tests,
+	CASE WHEN count(*) OVER(Partition by location Order by date ROWS 6 PRECEDING)> 6
+		THEN ISNULL(Round(AVG(new_tests)OVER(Partition by location Order by date ROWS 6 PRECEDING), 2), 0)
+		ELSE 0
+		END AS Avg_New_tests,
+	new_vaccinations,
+	CASE WHEN count(*) OVER(Partition by location Order by date ROWS 6 PRECEDING)> 6
+		THEN ISNULL(Round(AVG(new_vaccinations)OVER(Partition by location Order by date ROWS 6 PRECEDING), 2), 0)
+		ELSE 0
+		END AS Avg_New_Vaccinations
+
+FROM Portfolio.dbo.Covid_combined
+WHERE continent IS NOT NULL
+ORDER BY location
+
+;
+
+EXEC sp_weekly_rolling_average
+
